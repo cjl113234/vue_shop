e4864261b9f80d773c2ba612ef2db44a22fcc782
“Yo“cjl113234 <729258816@qq.com>
2020-06-17 17:03:19 +0800

    完成了用户列表功能

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- 2.为Echarts准备一个Dom -->
+    <el-row>
+      <el-col :span="6">
+        <h1>Welcome</h1>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+div {
+  margin: 0;
+  padding: 0;
+}
+</style>
